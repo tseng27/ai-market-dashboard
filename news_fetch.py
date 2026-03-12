9e8127445728b4b9fbe6718b5c4f6fd851c9ce38
tseng27 <107090519+tseng27@users.noreply.github.com>
2026-03-12 13:09:35 +0800

    Update news_fetch.py

--- news_fetch.py
+++ news_fetch.py
@@ -2,23 +2,25 @@ import feedparser
 import json
 
 sources = {
-    "yahoo":"https://finance.yahoo.com/news/rssindex",
-    "cnbc":"https://www.cnbc.com/id/100003114/device/rss/rss.html",
-    "marketwatch":"https://feeds.marketwatch.com/marketwatch/topstories/"
+    "Yahoo Finance":"https://finance.yahoo.com/news/rssindex",
+    "CNBC":"https://www.cnbc.com/id/100003114/device/rss/rss.html",
+    "MarketWatch":"https://feeds.marketwatch.com/marketwatch/topstories/"
 }
 
-news=[]
+news = []
 
-for name,url in sources.items():
+for source, url in sources.items():
+
     feed = feedparser.parse(url)
 
     for entry in feed.entries[:5]:
 
         news.append({
-            "title":entry.title,
-            "link":entry.link,
-            "source":name
+            "title": entry.title,
+            "link": entry.link,
+            "source": source
         })
 
 with open("news.json","w",encoding="utf-8") as f:
+
     json.dump(news,f,ensure_ascii=False,indent=2)
