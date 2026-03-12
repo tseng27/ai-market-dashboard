a00a33c2edcb20aa42a69676005a6947d55b3b23
tseng27 <107090519+tseng27@users.noreply.github.com>
2026-03-12 13:26:53 +0800

    Refactor news fetching to use a list of feeds

--- news_fetch.py
+++ news_fetch.py
@@ -1,26 +1,23 @@
 import feedparser
 import json
 
-sources = {
-    "Yahoo Finance":"https://finance.yahoo.com/news/rssindex",
-    "CNBC":"https://www.cnbc.com/id/100003114/device/rss/rss.html",
-    "MarketWatch":"https://feeds.marketwatch.com/marketwatch/topstories/"
-}
+feeds = [
+    "https://finance.yahoo.com/rss/topstories",
+    "https://www.cnbc.com/id/100003114/device/rss/rss.html",
+    "https://www.marketwatch.com/rss/topstories"
+]
 
 news = []
 
-for source, url in sources.items():
-
-    feed = feedparser.parse(url)
-
-    for entry in feed.entries[:5]:
-
+for feed in feeds:
+    data = feedparser.parse(feed)
+    for entry in data.entries[:5]:
         news.append({
             "title": entry.title,
-            "link": entry.link,
-            "source": source
+            "link": entry.link
         })
 
-with open("news.json","w",encoding="utf-8") as f:
+with open("news.json", "w", encoding="utf-8") as f:
+    json.dump(news, f, ensure_ascii=False, indent=2)
 
-    json.dump(news,f,ensure_ascii=False,indent=2)
+print("News updated")
